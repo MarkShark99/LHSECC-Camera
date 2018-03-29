47e0e6c7f9858c84cb0496e1e231fd0658370257
MarkShark99 <markdw88@gmail.com>
2018-03-29 13:01:45 -0400

    Add code to get temperature values from sensor

--- CarCamera.py
+++ CarCamera.py
@@ -1,7 +1,7 @@
 import sys
+
 import pygame
-import pygame.camera
-import pygame.font
+from lib import Adafruit_BME280
 
 pygame.init()
 pygame.display.init()
@@ -10,20 +10,25 @@ pygame.camera.init()
 size = (320, 240) # Resolution for PiTFT screen
 screen = pygame.display.set_mode(size, 0)
 cam_list = pygame.camera.list_cameras()
-font = pygame.font.Font(None, 30)
+font = pygame.font.Font(None, 20)
+
+thermometer = Adafruit_BME280.BME280(t_mode=Adafruit_BME280.BME280_OSAMPLE_8,
+                                     p_mode=Adafruit_BME280.BME280_OSAMPLE_8,
+                                     h_mode=Adafruit_BME280.BME280_OSAMPLE_8)
 
 cam = pygame.camera.Camera(cam_list[0], size)
 cam.start()
 
 while True:
+    temperature = thermometer.read_temperature_f() #Get temperature in fahrenheit
     image = cam.get_image()
     r = font.render("TEST", True, (255, 255, 255))
     screen.blit(image, (0, 0))
-    screen.blit(r, (100, 100))
+    screen.blit(r, (300, 220))
     
     pygame.display.update()
     
-    for event in pygame.event.get()
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             cam.stop()
             pygame.quit()
