af52f8dfffc82b9ecccebfcf63db0f4f21da41ea
Mark Washington <markdw88@gmail.com>
2018-12-29 01:52:15 -0500

    Updated documentation and refactored code

--- CarCamera.py
+++ CarCamera.py
@@ -1,3 +1,7 @@
+'''
+Written by Mark Washington
+''' 
+
 import sys
 
 import pygame
@@ -7,30 +11,29 @@ from lib import Adafruit_BME280, MCP3008
 VOLT_PORT = 1
 AMP_PORT = 3
 
-
 pygame.init()
 pygame.display.init()
 pygame.camera.init()
 
-size = (320, 240)  # Resolution for PiTFT screen
-screen = pygame.display.set_mode(size, 0)
+resolution = (320, 240)  # Resolution for PiTFT screen
+screen = pygame.display.set_mode(resolution, 0)
 cam_list = pygame.camera.list_cameras()
-font = pygame.font.Font(None, 20)
+overlay_font = pygame.overlay_font.overlay_font(None, 20)
 
 thermometer = Adafruit_BME280.BME280(t_mode=Adafruit_BME280.BME280_OSAMPLE_8,
                                      p_mode=Adafruit_BME280.BME280_OSAMPLE_8,
                                      h_mode=Adafruit_BME280.BME280_OSAMPLE_8)
 
-amp_or_something_sensor = MCP3008.MCP3008(clk=18, cs=25, miso=23, mosi=24)
+amp_volt_sensor = MCP3008.MCP3008(clk=18, cs=25, miso=23, mosi=24)
 
-cam = pygame.camera.Camera(cam_list[0], size)
+cam = pygame.camera.Camera(cam_list[0], resolution)
 cam.start()
 
 while True:
     try:
-        temperature = round(thermometer.read_temperature_f(), 1)  # Get temperature in fahrenheit
-        amps = round(amp_or_something_sensor.read_adc(AMP_PORT), 1)
-        volts = round(amp_or_something_sensor.read_adc(VOLT_PORT), 1)
+        temperature = round(thermometer.read_temperature_f(), 1)
+        amps = round(amp_volt_sensor.read_adc(AMP_PORT), 1)
+        volts = round(amp_volt_sensor.read_adc(VOLT_PORT), 1)
     except:
         temperature = "Error"
         amps = "Error"
@@ -38,9 +41,9 @@ while True:
     
     image = cam.get_image()
 
-    temperature_text = font.render("Temp  " + str(temperature), True, (255, 255, 255))
-    amperage_text = font.render("Amps: " + str(amps), True, (255, 255, 255))
-    voltage_text = font.render("Volt: " + str(volts), True, (255, 255, 255))
+    temperature_text = overlay_font.render("Temp  " + str(temperature), True, (255, 255, 255))
+    amperage_text = overlay_font.render("Amps: " + str(amps), True, (255, 255, 255))
+    voltage_text = overlay_font.render("Volt: " + str(volts), True, (255, 255, 255))
 
     screen.blit(image, (0, 0))  # Blit image from camera to screen
     screen.blit(temperature_text, (3, 190))  # Blit temperature to screen
@@ -49,7 +52,7 @@ while True:
 
     pygame.display.update()
 
-    for event in pygame.event.get():  # Get list of pygame events
+    for event in pygame.event.get():  # Get list of pygame events and check for request to quit
         if event.type == pygame.QUIT:
             cam.stop()
             pygame.quit()
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # LHSECC-Camera
-Stands for:\
+Stands for:
 > Lakeshore\
 > High\
 > School\
@@ -7,3 +7,31 @@ Stands for:\
 > Car\
 > Club\
 > Camera
+
+This program was requested by Lakeshore High School's electric car racing team for use during races to display a video feed and monitor the temperature and the voltage and amperage draws of the car via an onboard Raspberry Pi.
+
+## Components
+- Raspberry Pi Zero W - Main board
+- Raspberry Pi Camera Module - Camera for live video feed
+- Adafruit PiTFT - Screen used to display sensor values and video feed
+- Adafruit BME280 - Temperature sensor for monitoring the temperature of the car
+- Adafruit MCP3008 - Analog to digital converter for use with the voltage and amperage sensor
+
+## Features
+### Live video feed
+Using PyGame's camera module, the program is able to stream the camera feed directly to the screen. This was used to ensure better visibility behind the car during races.
+
+### Temperature monitoring
+Using an Adafruit BME280 temperature sensor, the program is able to monitor the temperature (in fahrenheit) of either the cabin or the motor and battery compartment of the car, and display it over the video feed.
+
+### Voltage and Amperage monitoring
+Using an Adafruit MCP3008 Analog-to-Digital convertor and another unknown sensor, the program is able to monitor the voltage and amperage being drawn by the car's motor, and display these values over the video feed. 
+
+## Unimplemented Features
+This is a list of features that were considered or worked on but were ultimately scrapped as this project had to be finished incredibly quickly while minimizing points of failure.
+
+### Wireless track timer and video feed
+Using a Wi-Fi network and a custom antenna attached to the Raspberry Pi, the program would be able to stream its video feed over the network and have a track timer that was controlled by a spectator. While the antenna was built and attached to a Raspberry Pi, that Pi ended up being broken by a member of the team and was replaced by one without the antenna. There was also the issue of range of Wi-Fi while on the track.
+
+### Video storage
+The program was originally going to store the live video feed to disk, but this idea was scrapped as continuously writing to the Pi's SD card could be a point of failure.
